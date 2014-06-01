f6c8f6669bef477ceaf886eb908dcbb60595ec51
Trever <dont@even.try>
2014-05-31 21:08:36 -0600

    These should have been in the last cl.

---
+++ indexer/src/parser/Parser.cpp
@@ -0,0 +1,18 @@
+/*
+ * Parser.cpp
+ *
+ *  Created on: May 31, 2014
+ *      Author: rever
+ */
+
+#include "Parser.h"
+
+Parser::Parser() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Parser::~Parser() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ indexer/src/parser/Parser.h
@@ -0,0 +1,17 @@
+/*
+ * Parser.h
+ *
+ *  Created on: May 31, 2014
+ *      Author: rever
+ */
+
+#ifndef PARSER_H_
+#define PARSER_H_
+
+class Parser {
+public:
+	Parser();
+	virtual ~Parser();
+};
+
+#endif /* PARSER_H_ */
